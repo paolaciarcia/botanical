580bf4230ff1d169058be826f3d8dfa69652724d
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-06-13 19:24:14 -0300

    Add project

---
+++ Botanical/Botanical/BotanicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotanicalApp.swift
+//  Botanical
+//
+//  Created by Paola Golombieski Ciarcia on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BotanicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
