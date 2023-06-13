6e2f45acbe10a84df496496dea3676c1246e314c
Erick Borges <erick.borges@stone.com.br>
2023-06-13 20:57:40 -0300

    feat: style and add content to collection cell

--- Botanical/Botanical/BotanicalApp.swift
+++ Botanical/Botanical/BotanicalApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BotanicalApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PlantCollectionView()
         }
     }
 }
--- Botanical/Botanical/PlantCollectionView.swift
+++ Botanical/Botanical/PlantCollectionView.swift
@@ -10,15 +10,32 @@ import SwiftUI
 
 struct PlantCollectionView: View {
     let colums = [
-        GridItem(.adaptive(minimum: 170))
+        GridItem(.adaptive(minimum: 170), spacing: 16)
     ]
 
     var body: some View {
         ScrollView(.vertical) {
-            LazyVGrid(columns: colums, spacing: 22) {
-                ForEach(0...300, id: \.self) { _ in
-                    Color.red.frame(width: 170, height: 170)
-                    Color.green.frame(width: 170, height: 170)
+            LazyVGrid(columns: colums, spacing: 16) {
+                ForEach(0...60, id: \.self) { _ in
+                    // cell
+                    ZStack {
+                        Image("placeholder-plant")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        
+                        VStack {
+                            Spacer()
+                            Text("Botanical")
+                                .multilineTextAlignment(.center)
+                                .frame(maxWidth: .infinity, minHeight: 40)
+                                .background(Gradient(colors: [.clear, .green]))
+                        }
+                    }
+                    .cornerRadius(16)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                            .stroke(Color.green, lineWidth: 4)
+                    }
                 }
             }
             .padding()
